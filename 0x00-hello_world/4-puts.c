2ecd2a00b710f02c2ba295afff76a5976ce541e2
Enahoro360 <enahorodaniel360@gmail.com>
2023-06-15 20:31:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.n>
 /**
- * main - A programm that prints a line with put functio
- * 
- * Return: 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
